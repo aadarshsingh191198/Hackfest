00a7ed5e563419d0e4ecbd4af5bd075e79795a62
Aadarsh Singh <aadarshsingh191198@gmail.com>
2018-08-10 19:34:01 +0530

    Update main.js

--- Hackfest_files/preloader/main.js
+++ Hackfest_files/preloader/main.js
@@ -2,9 +2,9 @@
 
 $(document).ready(function() {
 
-	setTimeout(function(){
+// 	setTimeout(function(){
 		$('body').addClass('loaded');
 		$('h1').css('color','#222222');
-	}, 3000);
+// 	}, 3000);
 
 });
